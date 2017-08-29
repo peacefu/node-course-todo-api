6e45138744e9e9d66badba8bf1ab57ab70ee0edb
Alex Tse <ax@Alexs-MacBook-Pro.local>
2017-08-29 20:20:58 +0800

    Setup app for heroku4

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,12 +3,12 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-// let db = {
-//   localhost: 'mongodb://localhost:27017/TodoApp',
-//   mlab: 'mongodb://peacefu-dbuser:peacefu-dbuser123123@ds161503.mlab.com:61503/todoapp20170829'
-// };
-// mongoose.connect(db.localhost || db.mlab);
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://peacefudbuser:peacefudbuser123123@ds161503.mlab.com:61503/todoapp20170829'
+};
+mongoose.connect(process.env.PORT?:db.mlab || db.localhost);
 
-mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/TodoApp');
+//mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
