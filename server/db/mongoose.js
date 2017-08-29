4a9a0111f77ddad915bd7740bfe93c9d75514fb2
Alex Tse <ax@Alexs-MacBook-Pro.local>
2017-08-29 20:23:48 +0800

    Setup app for heroku5

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -7,7 +7,7 @@ let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
   mlab: 'mongodb://peacefudbuser:peacefudbuser123123@ds161503.mlab.com:61503/todoapp20170829'
 };
-mongoose.connect(process.env.PORT?:db.mlab || db.localhost);
+mongoose.connect(process.env.PORT?db.mlab : db.localhost);
 
 //mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/TodoApp');
 
