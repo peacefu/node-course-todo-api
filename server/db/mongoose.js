74a7891533cb71cb71fe00b4c36e44f00925cd2c
Alex Tse <ax@Alexs-MacBook-Pro.local>
2017-08-29 19:16:23 +0800

    Setup app for heroku2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,10 +2,13 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://peacefu:pm123123!@#$@ds161503.mlab.com:61503/todoapp20170829'
-};
-mongoose.connect(db.localhost || db.mlab);
+
+// let db = {
+//   localhost: 'mongodb://localhost:27017/TodoApp',
+//   mlab: 'mongodb://peacefu-dbuser:peacefu-dbuser123123@ds161503.mlab.com:61503/todoapp20170829'
+// };
+// mongoose.connect(db.localhost || db.mlab);
+
+mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
